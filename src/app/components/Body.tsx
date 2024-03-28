c7a1e12c2b14c68d85eaebea3261a2f5049be6af
Ken Andrew Taylor <kentaylorappdev@gmail.com>
2024-03-28 17:18:30 -0400

    featured almost done

--- src/app/components/Body.tsx
+++ src/app/components/Body.tsx
@@ -2,15 +2,20 @@ import React from 'react'
 import SearchInput from './SearchInput'
 import { IoIosArrowBack } from "react-icons/io";
 import NavigatorComponent from './NavigatorComponent';
+import FeaturedArtist from './FeaturedArtist';
 const Body = () => {
   return (
     <div className='flex flex-col flex-1 text-white bg-mytheme-black pt-8 px-6'>
+      {/* NOTE: this is thew top div */}
       <div className='flex flex-row w-full h-16 bg-mytheme-black justify-between items-center '>
         <div className='p-4 bg-mytheme-darkBackground h-[50px] w-[50px]'>
           <IoIosArrowBack />
         </div>
         <NavigatorComponent />
         <SearchInput />
+      </div>
+      <div className='pt-8'>
+        <FeaturedArtist />
       </div>
     </div>
   )
---
+++ src/app/components/FeaturedArtist.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import Image from 'next/image'
+import { RiVerifiedBadgeFill } from "react-icons/ri";
+import { ImHeadphones } from "react-icons/im";
+const FeaturedArtist = () => {
+  return (
+    <div className="flex flex-row w-full h-72 p-0 bg-mytheme-black">
+      <div className="flex flex-col items-start justify-center w-1/2 ">
+        <div className="flex flex-col gap-4 pl-8">
+          <div className='flex flex-row gap-2'>
+            <RiVerifiedBadgeFill className='text-green-500 text-2xl' />
+            <p className='text-mytheme-greyText'>Verified Artist</p>
+          </div>
+          <p className="text-4xl font-bold text-white">Ruth B</p>
+          <div className="flex flex-row gap-2 justify-center items-center">
+            <ImHeadphones />
+            <p className="text-white">82,736,050</p>
+            <p className="text-mytheme-greyText">monthly listeners</p>
+          </div>
+        </div>
+      </div>
+      <div className="w-1/2">
+        <Image
+          src="/assets/images/ruth2.jpg"
+          alt="artiste"
+          className="w-full h-full object-cover rounded-md"
+          width={300}
+          height={300}
+        />
+      </div>
+    </div>
+  )
+}
+
+export default FeaturedArtist

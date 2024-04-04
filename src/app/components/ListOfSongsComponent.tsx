73027d1c3005e0f55d374f7c1a4cbabd38decc8a
Ken Andrew Taylor <kentaylorappdev@gmail.com>
2024-04-03 20:10:16 -0400

    customized the scrollbar

--- src/app/components/ListOfSongsComponent.tsx
+++ src/app/components/ListOfSongsComponent.tsx
@@ -4,7 +4,7 @@ import { Table, TableBody, TableHeader } from "@/components/ui/table";
 
 const ListOfSongsComponent = () => {
   const getRandomPictureUrl = (baseUrl: string) => {
-    const randomNumber = Math.floor(Math.random() * 1000); // Generate a random number between 0 and 999
+    const randomNumber = Math.floor(Math.random() * 1000);
     return `${baseUrl}?random=${randomNumber}`;
   };
 
@@ -12,13 +12,16 @@ const ListOfSongsComponent = () => {
   const initialSongs = songs.slice(0, 4);
 
   const scrollableTableStyles: React.CSSProperties = {
-    maxHeight: '300px', // Adjust the height as per your requirement
-    overflowY: 'scroll', // Ensure scrolling even when not necessary
+    maxHeight: '400px',
+    overflowY: 'scroll',
   };
 
+  // NOTE: So table-fixed made a vexy big difference to the width of the app, not sure if it's a breaking one but it kinda looks ok.
+  // it takse up all the scren i originally didn't plan to do this.
+  // removed table-fixed
   return (
-    <div style={scrollableTableStyles}>
-      <Table>
+    <div style={scrollableTableStyles} className="scrollable-table">
+      <Table className="w-full ">
         <TableBody>
           {songs.map((song: Song) => (
             <SongComponentRow
